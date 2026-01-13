fbe981bbfc9dfebd12b49f7de75f50d184d0e0bb
OpenSorcerer20 <opensorcerer20@gmail.com>
2026-01-12 21:31:55 -0600

    add env config

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import 'dotenv/config'
 
+import { PORT } from './config/env.js'
 import app from './src/app.js'
 
-const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
